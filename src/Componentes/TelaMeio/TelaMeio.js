66a82c96600a3cfc45501874b1043556899ddb2f
GabrielSales <104046747+Usales@users.noreply.github.com>
2025-07-03 15:46:48 -0300

    Ajuste do posicionamento do scroll-indicator para evitar sobreposição com o botão Currículo e melhorias gerais de responsividade

--- src/Componentes/TelaMeio/TelaMeio.js
+++ src/Componentes/TelaMeio/TelaMeio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './TelaMeio.css';
 import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaJava, FaDatabase, FaGitAlt } from 'react-icons/fa';
-import { SiTypescript, SiPostgresql, SiAngular, SiTailwindcss, SiMongodb, SiGithub, SiVite } from 'react-icons/si';
+import { SiTypescript, SiPostgresql, SiAngular, SiTailwindcss, SiMongodb, SiGithub, SiVite, SiStyledcomponents } from 'react-icons/si';
 import projeto1 from '../Imagens/projeto1.png';
 import projeto2 from '../Imagens/projeto2.png';
 import projeto3 from '../Imagens/projeto3.png';
@@ -77,6 +77,7 @@ const TelaMeio = () => {
         { nome: "React.js", icone: <FaReact /> },
         { nome: "Angular", icone: <SiAngular /> },
         { nome: "Tailwind", icone: <SiTailwindcss /> },
+        { nome: "Styled Components", icone: <SiStyledcomponents /> },
       ]
     },
     {
@@ -85,6 +86,7 @@ const TelaMeio = () => {
         { nome: "Git", icone: <FaGitAlt /> },
         { nome: "GitHub", icone: <SiGithub /> },
         { nome: "Vite", icone: <SiVite /> },
+        { nome: "Styled Components", icone: <SiStyledcomponents /> },
       ]
     }
   ];
